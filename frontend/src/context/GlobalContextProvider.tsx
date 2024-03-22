c802202cd0874c3cfab7d86e916bdcc9d57a70eb
alex-nicaise <atnicais@gmail.com>
2024-03-22 17:24:22 -0400

    Some changes not pushed

--- frontend/src/context/GlobalContextProvider.tsx
+++ frontend/src/context/GlobalContextProvider.tsx
@@ -1,6 +1,5 @@
 import { createContext, useEffect, useState } from "react";
 import authenticateUser from "../utils/auth";
-import { PulseLoader } from "react-spinners";
 
 type GlobalContextType = {
   isAuthenticated: boolean;
--- frontend/src/pages/ProtectedRoutes/ProtectedRoutes.tsx
+++ frontend/src/pages/ProtectedRoutes/ProtectedRoutes.tsx
@@ -5,8 +5,6 @@ const ProtectedRoutes = () => {
   const { isAuthenticated } = useAuthContext();
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-
-  //TODO: STOP MAKING DASHBOARD AND SIGN IN PING BACK AND FORTH
 };
 
 export default ProtectedRoutes;
