6f107644f62c54114a382b65e4b1eb7c601b24fe
alex-nicaise <atnicais@gmail.com>
2024-04-08 18:16:36 -0400

    Integrate sidebar and main responsiveness

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Global.css";
+import "./pages/Dashboard/Dashboard.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard.tsx";
 import SignUp from "./pages/Sign-Up/SignUp.tsx";
--- frontend/src/pages/Dashboard/DashboardLayout.tsx
+++ frontend/src/pages/Dashboard/DashboardLayout.tsx
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import useGlobalContext from "../../context/useGlobalContext";
 import Header from "../../ui/Header";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const { sessionUser } = useGlobalContext();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const onSideBarToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setSidebarOpen(!sidebarOpen);
+  };
 
   return (
     <>
-      <Header sessionUser={sessionUser} />
+      <Header sessionUser={sessionUser} onSideBarToggle={onSideBarToggle} />
 
-      <main className="flex flex-col w-full h-full bg-gray-200 dark:bg-gray-900">
-        {children}
-      </main>
-
-      <aside>
-        <h2>this is a sidebar</h2>
-      </aside>
+      <section className="flex h-full" id="notes-main-sidebar-wrapper">
+        <aside
+          id="notes-list-sidebar"
+          className={
+            sidebarOpen
+              ? "sidebar-open flex flex-col h-full bg-gray-300 text-black"
+              : "sidebar-close flex flex-col h-full bg-gray-300 text-black"
+          }
+        >
+          <h2 className="text-xl font-bold mt-10 ml-10 mb-6">Notes</h2>
+          <ul>
+            <li className="mb-3 hover:bg-gray-400 py-3 px-10">
+              <h4 className="font-bold">Note One</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+            </li>
+          </ul>
+        </aside>
+        <main
+          id="main-notes-view"
+          className={
+            sidebarOpen
+              ? "sidebar-open flex flex-col h-full bg-white dark:bg-gray-900 items-center w-full"
+              : "sidebar-close flex flex-col h-full bg-white dark:bg-gray-900 items-center w-full"
+          }
+        >
+          <div className="flex flex-col w-full justify-center width-container p-10">
+            {children}
+          </div>
+        </main>
+      </section>
     </>
   );
 };
